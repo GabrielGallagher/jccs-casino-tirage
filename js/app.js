599998d690f31d9bed7920d4946e0fccaededc7e
Nicolas Lupien <niclupien@gmail.com>
2016-03-07 12:47:55 -0500

    first comit

---
+++ js/app.js
@@ -0,0 +1,106 @@
+$.fn.extend({
+  animateCss: function (animationName) {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+      $(this).removeClass('animated ' + animationName);
+    });
+  }
+});
+
+
+function animate(n, fn) {
+  var id = setInterval(function () {
+    var i = Math.floor(Math.random()*window.names.length);
+    var name = window.names[i];
+    $('#winner').html(name);
+
+    if (--n <= 0) {
+      clearInterval(id);
+      fn();
+    }
+  }, 150);
+}
+
+function draw() {
+  console.log('# STARTING A DRAW #');
+
+  // do nothing if there's no names
+  if (!window.names || !window.names.length) {
+    console.log('no names loaded');
+    $('#winner').html('Aucune donnée');
+    $('#start').text('I\'m Feeling Lucky');
+    return;
+  }
+
+  // pick a random name
+  var n = Math.floor(Math.random()*window.names.length);
+  var name = window.names[n];
+
+  // audit in the console
+  console.log('names in the hat', window.names.length);
+  console.log('picked number', n, '('+name+')');
+
+  // show winner
+  $('#winner').html(name);
+  $('#winner').animateCss('bounce');
+  $('#start').text('I\'m Feeling Lucky Again');
+
+  // remove winner from names
+  remove(name, window.names);
+}
+
+function remove(e, arr) {
+    var found = arr.indexOf(e);
+
+    while (found !== -1) {
+        arr.splice(found, 1);
+        found = arr.indexOf(e);
+    }
+}
+
+function flattenEntries(entries) {
+  return entries.reduce(function (prev, entry) {
+    for (var i = 0; i < entry.chances; i++)
+      prev.push(entry.name);
+    return prev;
+  }, []);
+}
+
+function dataToEntry(raw) {
+  return {
+    name: raw['gsx$nom']['$t'],
+    chances: raw['gsx$chances']['$t']
+  };
+}
+
+function refresh() {
+  $('#winner').text('Tirage');
+  var source = 'https://spreadsheets.google.com/feeds/list/1j5rYcIyj-RzXd8JekdXUuve65LlrlEb4ypQx6X4zxzM/od6/public/values?alt=json-in-script&callback=?';
+
+  $('#start').text('Loading names...');
+
+  $.getJSON(source, function(data) {
+    var entries = data.feed.entry.map(dataToEntry);
+    window.names = flattenEntries(entries);
+    $('#start').text('I\'m Feeling Lucky');
+  });
+}
+
+$(function() {
+  // window.names = ['Nicolas', 'Josée', 'Naomie', 'Julie', 'Jean', 'Paul', 'Pierre'];
+  window.names = [];
+
+  refresh();
+
+  $('#start').on('click', function () {
+    $('#start').text('wait for it...');
+    $('#start').blur();
+    animate(50, draw);
+  });
+
+  $('#refresh').on('click', function() {
+    refresh();
+  });
+});
+
+
